9bf991ab80b962807ae87074e60eecd6f3e513c9
nehalali110 <nehalalikhan110@gmail.com>
2025-12-31 16:04:30 +0500

    working on the app logic

--- main.py
+++ main.py
@@ -1,16 +1,33 @@
 import sys
+from requests import Request
 import uvicorn
 from utils.helpers import validate_arguments
 from pydantic import ValidationError
+from requests.exceptions import RequestException
+from fastapi import FastAPI
+from fastapi.responses import Response
+
+
+app = FastAPI()
+
+@app.get("/{full_path:path}")
+def test_endpoint(full_path:str, request: Request):
+    return Res
 
 def main():
     try:
         port, remote_srvr = validate_arguments(sys.argv)
         print(f"Port: {port} Remote Server: {remote_srvr}")
+        print("Starting the server...")
+        uvicorn.run("main:app", port=port ,reload=True)
 
     except (IndexError, ValidationError, ValueError):
         print("Usage: caching-proxy --port <number> --origin <url>")
         return
+
+    except RequestException:
+        remote_url = sys.argv[4]
+        print(f"Invalid origin url: {remote_url}")
 
 if __name__ == "__main__":
     main()
--- utils/helpers.py
+++ utils/helpers.py
@@ -1,12 +1,21 @@
+import sys
+#models module is not the path therefore add it
+models_path = "/home/nehal/workspace/github/cachingproxy"
+sys.path.insert(0,models_path)
+
+
+
 from models.args_model import arguments_model
 from pydantic import ValidationError, BaseModel
 import requests
 
+
+
 def validate_arguments(args):
 	if not len(args) == 5 or not args[1] == "--port" or not args[3] == "--origin":
 		raise ValueError
-
-	arg_obj = arguments_model(port=args[2], remote_url = args[4])
+	arg_obj = arguments_model(port=args[2], remote_url=args[4])
+	validate_domain_connectivity(arg_obj.remote_url)
 	return arg_obj.port, arg_obj.remote_url
 
 
@@ -14,5 +23,14 @@ def validate_domain_connectivity(url):
 	r = requests.get(url)
 	print(r)
 
-temp_url = "https://google.com"
-print(validate_domain_connectivity(temp_url))
+
+def validate_port_connectivity(port):
+	localhost = "http://127.0.0.1"
+	r = requests.get(f"{localhost}:{port}")
+	print(r)
+
+# print(validate_port_connectivity(8000))
+
+# temp_url = "https://invaliddomain.com"
+# print(validate_domain_connectivity(temp_url))
+# print("helper module running")
